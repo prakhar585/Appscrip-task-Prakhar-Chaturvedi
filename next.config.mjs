dae2b88509f90b969ddebedccd4b913360ce2391
unknown <prakharchaturvedi9860@gmail.com>
2025-06-22 18:59:16 +0530

    final draft again

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
   async redirects() {
     return [
       {
         source: '/',
         destination: '/products',
-        permanent: true, // Use false for temporary redirects
+        permanent: true,
       },
     ]
   },
